935b4fd3f9c82519d833f0e9491917f69ed42048
heqro <58432067+heqro@users.noreply.github.com>
2019-12-02 16:01:58 +0100

    Add files via upload

---
+++ IllegalMovementException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hr.iglesias.2018
+ */
+class IllegalMovementException extends Exception {
+    public IllegalMovementException(){
+        super("Posicion ilegal");
+    }
+}
---
+++ Pieza.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hr.iglesias.2018
+ */
+public abstract class Pieza {
+    private Posicion pos;
+    private Color color;
+    private String nombre;
+    public abstract void Mover(Posicion p) throws IllegalMovementException;
+    public abstract String getColor();
+    @Override
+    public abstract String toString();
+}

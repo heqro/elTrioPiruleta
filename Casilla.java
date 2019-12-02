8e3bb315bb1cfa7104fe0fa9b0022ae6b30d74c6
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-02 16:00:04 +0100

    Create Casilla.java

---
+++ Casilla.java
@@ -0,0 +1,49 @@
+package p_final;
+
+/**
+ *
+ * @author j.montes.2018
+ */
+public class Posicion {
+    int coordenadax;
+    char coordenaday;
+    
+    
+    public Posicion(int x, char y, boolean l){
+        coordenadax=x;
+        coordenaday=y;
+        
+    }
+    public void setCoordenadax(int x){
+        coordenadax=x;
+    }
+    public void setCoordenaday(char y){
+        coordenaday=y;    
+    }
+    
+    public int getCoordenadax(){
+        return coordenadax;
+    }
+    public char getCoordenaday(){
+        return coordenaday;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if (o==null){
+            return false;
+        }
+        if (getClass() != o.getClass()){
+            return false;
+        }
+        final Posicion pos = (Posicion) o;
+        if (this.coordenadax != pos.coordenadax){
+            return false;
+        }else{
+            if (this.coordenaday != pos.coordenaday){
+                return false;
+            } else return true;
+        }
+        
+              
+    }

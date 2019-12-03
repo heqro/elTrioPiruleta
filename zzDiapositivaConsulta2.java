3376442b8aeb4fde23ed3ff951b5b77b3a2457ab
heqro <58432067+heqro@users.noreply.github.com>
2019-12-03 10:22:40 +0100

    Update zzDiapositivaConsulta2.java

--- zzDiapositivaConsulta2.java
+++ zzDiapositivaConsulta2.java
@@ -5,18 +5,30 @@ public class miConsultaSQL{
   
   public static void main(String[] args){
     Connection con = null;
-    Statement rs = null; 
+    Statement st = null;
+    ResultSet rs = null;
+    String miConsulta = "SELECT DNI, Nombre, Apellidos, FechaNac From ESTUDIANTES");
     try{
       con = DriverManager.getConnection(URL, USER, PASSWORD);
-      System.out.println("Conexion establecida");
-    }catch(SQLException e){
+      st = con.createStatement();
+      rs = st.executeQuery(miConsulta);
+      while(rs.next()){
+       System.out.println("DNI: " + rs.getInt(1)
+                          + "\n Nombre: " + rs.getString(2) 
+                          + "\n Apellidos: " + rs.getString(3)
+                          + "\n FechaNac: " + rs.getDate(4)
+                          + "\n ----------------------------------------");
+      }
+    }catch(SQLException e){ //excepción provocada por error de acceso a la base de datos
       e.printStackTrace();
     } finally {
       try{
+        st.close();
         con.close();
       }catch (Exception e){
         e.printStackTrace();
       }
   }
 }
+
 

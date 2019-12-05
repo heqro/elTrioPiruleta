73a51a7ac2ff6d29f311a76382b9eaa41df6c0d9
Patilla4puto <58432399+Patilla4puto@users.noreply.github.com>
2019-12-05 18:38:26 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -11,11 +11,42 @@ package p_final;
  */
 public class Tablero {
     private Pieza Marcador[][] = new Pieza[8][8];
-    
+    public Tablaro(){
+        int i;
+        int j;
+        for (i=0;i==7;i++){
+            for(j=0;j==7;j++){
+                Marcador[i][j] = null;
+            }
+        };
+    }
     public Tablero(Pieza M[][]){
         Marcador= M;
         
     }
+    private ArrayList<Posicion> BuscarPiezas(Char s){
+        ArrayList<Posicion>  p = new ArrayList<Posicion>;
+        int i;
+        int j;
+        for (i = 0; i==7; i++){
+            for (j=0;j==7;j++){
+                if (Marcador[i][j].getNombre == s){
+                    p.add(new Posicion(i,j));
+                }
+            }
+        };
+        return(p);
+        
+    }
+    public Pieza MovimientoValido(String s){
+      char a =  s.charAt(1);
+      this.BuscarPiezas(a)
+    }
+    public void MoverPieza (Posicion posM, Pieza p ){
+        try{
+        }catch()
+    }
+    
     private void insertarPieza(String pstring, int x, int y){
         switch(pstring.charAt(0)){
             case 'R':

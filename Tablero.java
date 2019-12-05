1c5dbea8c601f93b06166b988a98c6e17d3dd333
heqro <58432067+heqro@users.noreply.github.com>
2019-12-05 20:23:56 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -11,55 +11,171 @@ package p_final;
  */
 public class Tablero {
     private Pieza Marcador[][] = new Pieza[8][8];
-    private static int contadorRey = 0;
-    private static int contadorAlfil = 0;
-    private static int contadorPeon = 0;
-    private static int contadorDama = 0;
-    private static int contadorTorre = 0;
-    private static int contadorCaballo = 0;
+    private int contReyNegro, contReyBlanco = 0;
+    private int contAlfilBlancoCasillasBlancas, contAlfilBlancoCasillasNegras,
+            contAlfilNegroCasillasBlancas, contAlfilNegroCasillasNegras= 0;
+    private int contPeonNegro, contPeonBlanco = 0;
+    private int contDamaBlanca, contDamaNegra = 0;
+    private int contTorreBlanca, contTorreNegra = 0;
+    private int contCaballoNegro, contCaballoBlanco = 0;
+    private boolean tableroIlegal = false;
     public Tablero(Pieza M[][]){
         Marcador = M;
     }
+    
+    public void comprobarTableroLegal(){
+        boolean legalReyes = contReyNegro == 1 && contReyBlanco == 1;
+        boolean legalPeones = contPeonNegro <= 8 && contPeonBlanco <= 8;
+        boolean legalDamas = contDamaNegra <= 1 && contDamaBlanca <= 1;
+        boolean legalTorres = contTorreNegra <= 2 && contTorreBlanca <= 2;
+        boolean legalCaballos = contCaballoNegro <= 2 && contCaballoBlanco <= 2;
+        boolean legalAlfiles = contAlfilBlancoCasillasBlancas <= 1 && contAlfilBlancoCasillasNegras <= 1
+                && contAlfilNegroCasillasBlancas <= 1 && contAlfilNegroCasillasNegras <= 1; 
+        tableroIlegal = legalReyes && legalPeones && legalDamas && legalTorres && legalCaballos && legalAlfiles && !tableroIlegal;
+    }
+    
     public void insertarPieza(String pstring, Posicion p){
         int x = p.getCoordenadax();
         int y = p.getCoordenaday();
+        char colorPieza = pstring.charAt(1);
         switch(pstring.charAt(0)){
             case 'R':
             {
-                contadorRey++;
-                Marcador[x][y] = new Rey(pstring.charAt(1));
+                switch(colorPieza){
+                    case 'B':case 'b':{
+                        contReyBlanco++;
+                        break;
+                    }
+                    case 'N':case 'n':{
+                        contReyNegro++;
+                        break;
+                    }
+                    default:{//color de pieza ilegal
+                        tableroIlegal = true;
+                    }
+                }
+                Marcador[x][y] = new Rey(colorPieza);
+                break;
             }
             case 'A':
             {
-                contadorAlfil++;
-                Marcador[x][y] = new Alfil(pstring.charAt(1));
+                switch(colorPieza){
+                    case 'B': case 'b':{
+                        if((x + y) % 2 == 0){
+                            contAlfilBlancoCasillasNegras++;
+                            /*El alfil blanco de casillas negras se mueve por 
+                            coordenadas x, y cuya suma da un número par. */
+                        }
+                        else{
+                            contAlfilBlancoCasillasBlancas++;
+                            /*El alfil blanco de casillas blancas se mueve por 
+                            coordenadas x, y cuya suma da un número par. */
+                        }
+                        break;
+                    }
+                    case 'N': case 'n':{
+                        if((x + y) % 2 == 0){
+                            contAlfilNegroCasillasNegras++;
+                            /*El alfil negro de casillas negras se mueve por 
+                            coordenadas x, y cuya suma da un número par. */
+                        }
+                        else{
+                            contAlfilNegroCasillasBlancas++;
+                            /*El alfil negro de casillas blancas se mueve por 
+                            coordenadas x, y cuya suma da un número par. */
+                        }
+                        break;
+                    }
+                    default:{
+                        tableroIlegal = true;
+                    }
+                    
+                }
             }
             case 'T':
             {
-                contadorTorre++;
-                Marcador[x][y] = new Torre(pstring.charAt(1));
+                switch(colorPieza){
+                    case 'B':case 'b':{
+                        contTorreBlanca++;
+                        break;
+                    }
+                    case 'N':case 'n':{
+                        contTorreNegra++;
+                        break;
+                    }
+                    default:{//color de pieza ilegal
+                        tableroIlegal = true;
+                    }
+                }
+                Marcador[x][y] = new Torre(colorPieza);
+                break;
             }
             
             case 'D':
             {
-                contadorDama++;
-                Marcador[x][y] = new Dama(pstring.charAt(1));
+                switch(colorPieza){
+                    case 'B':case 'b':{
+                        contDamaBlanca++;
+                        break;
+                    }
+                    case 'N':case 'n':{
+                        contDamaNegra++;
+                        break;
+                    }
+                    default:{//color de pieza ilegal
+                        tableroIlegal = true;
+                    }
+                }
+                Marcador[x][y] = new Dama(colorPieza);
+                break;
             }
             case 'P':
             {
-                contadorPeon++;
-                Marcador[x][y] = new Peon(pstring.charAt(1));
+                switch(colorPieza){
+                    case 'B':case 'b':{
+                        contPeonBlanco++;
+                        break;
+                    }
+                    case 'N':case 'n':{
+                        contPeonNegro++;
+                        break;
+                    }
+                    default:{//color de pieza ilegal
+                        tableroIlegal = true;
+                    }
+                }
+                Marcador[x][y] = new Peon(colorPieza);
+                break;
             }
             case 'C':
             {
-                contadorCaballo++;
-                Marcador[x][y] = new Caballo(pstring.charAt(1));
+                switch(colorPieza){
+                    case 'B':case 'b':{
+                        contCaballoBlanco++;
+                        break;
+                    }
+                    case 'N':case 'n':{
+                        contCaballoNegro++;
+                        break;
+                    }
+                    default:{//color de pieza ilegal
+                        tableroIlegal = true;
+                    }
+                }
+                Marcador[x][y] = new Rey(colorPieza);
+                break;
             }
-            default: //caso de 'V'
+            case 'V':
             {
                 Marcador[x][y] = null;
+                break;
+            }
+            default: //nombre de pieza ilegal
+            {
+                tableroIlegal = true;
             }
         }
         
     }
 }
+

52e42872af2db0077781bcab12812c5a23b378e7
heqro <58432067+heqro@users.noreply.github.com>
2019-12-05 18:54:17 +0100

    Update Caballo.java

--- Caballo.java
+++ Caballo.java
@@ -12,9 +12,4 @@ public class Caballo extends Pieza{
     public ArrayList<Posicion> calcularMovimientos(Tablero t) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    @Override
-    public void Mover(Posicion p) throws IllegalMovementException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }

9bce013fe2605e7031e32e9e0a49852ba50ae876
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-05 18:48:49 +0100

    Create ModeloUsuario.java

---
+++ ModeloUsuario.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p_final;
+
+/**
+ *
+ * @author juans
+ */
+public class ModeloUsuario {
+    private Modelo Modelo;
+    private boolean Resuelto ;
+    private int contadorIntentos,contadorErrores;
+    
+    public Modelo getModelo(){
+        return Modelo;
+    }
+    public boolean getResuelto(){
+        return Resuelto;
+    }
+    public int getIntentos(){
+        return contadorIntentos;
+    }
+    public int getErrores(){
+        return contadorErrores;
+    }
+    public void setIntentos(int i){
+        contadorIntentos=i;
+    }
+    public void setErrores(int i){
+        contadorErrores=i;
+    }
+    public void setResuelto(boolean i){
+        Resuelto=i;
+    }
+    
+    
+}

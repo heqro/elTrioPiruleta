a9bd36138a2a771f5af8bf109a1758eab39c8e99
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-23 12:53:07 +0100

    Update Alfil.java

--- Alfil.java
+++ Alfil.java
@@ -9,11 +9,53 @@ public class Alfil extends Pieza{
      }
 
     public ArrayList<Posicion> calcularMovimientos(Tablero t) {
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public ArrayList<Posicion> calcularMovimientos() {
+    public ArrayList<Posicion> calcularMovimientos(Posicion coordenada) {
+        ArrayList<Posicion> aposiciones = new ArrayList();
+        int x  = coordenada.getCoordenadax();
+        char y = coordenada.getCoordenaday();
+        
+        aposiciones.add(coordenada);
+        
+        while( x < 8 ||  y< 'h'){ //diagonal sup derecha
+            x= x+1;
+            int yn= Character.getNumericValue(y);
+            yn=yn + 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        while( x < 8 ||  y> 'a'){ //diagonal sup izda
+            x= x+1;
+            int yn= Character.getNumericValue(y);
+            yn=yn - 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        while( x > 1 ||  y< 'h'){ //diagonal inf derecha
+            x= x-1;
+            int yn= Character.getNumericValue(y);
+            yn=yn + 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        while( x > 1 ||  y> 'a'){ //diagonal inf izquierda
+            x= x-1;
+            int yn= Character.getNumericValue(y);
+            yn=yn - 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

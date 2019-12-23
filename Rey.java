4f86b3d9aa3a324dde07dc8d8a144994d0b8a92c
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-23 13:32:08 +0100

    Update Rey.java

--- Rey.java
+++ Rey.java
@@ -12,7 +12,25 @@ public class Rey extends Pieza{
     }
 
     @Override
-    public ArrayList<Posicion> calcularMovimientos() {
+    public ArrayList<Posicion> calcularMovimientos(Posicion coordenada) {
+        ArrayList<Posicion> aposiciones = new ArrayList();
+        int x  = coordenada.getCoordenadax();
+        char y = coordenada.getCoordenaday();
+        char y1 = (char)(Character.getNumericValue(y)+1);
+        char y2 = (char)(Character.getNumericValue(y)+1);
+        if (y<'h')aposiciones.add(new Posicion (x,y1));
+        if (y>'a')aposiciones.add(new Posicion (x,y2));
+        aposiciones.add(new Posicion (x,y));
+        if(y<'h' && x<8)aposiciones.add(new Posicion (x+1,y1));
+        if(y>'a' && x<8)aposiciones.add(new Posicion (x+1,y2));
+        if(x<8)aposiciones.add(new Posicion (x+1,y));
+        if(x>1)aposiciones.add(new Posicion (x-1,y));
+        if(y<'h' && x>1)aposiciones.add(new Posicion (x-1,y1));
+        if(y>'a' && x>1)aposiciones.add(new Posicion (x-1,y2));
+        
+        
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     

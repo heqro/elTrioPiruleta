080d8b7b5187080dd7f26319c90b916ab6526cc7
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-23 12:54:08 +0100

    Update Torre.java

--- Torre.java
+++ Torre.java
@@ -12,7 +12,30 @@ public class Torre extends Pieza{
     }
 
     @Override
-    public ArrayList<Posicion> calcularMovimientos() {
+    public ArrayList<Posicion> calcularMovimientos(Posicion coordenada) {
+         ArrayList<Posicion> aposiciones = new ArrayList();
+        int x  = coordenada.getCoordenadax();
+        char y = coordenada.getCoordenaday();
+        
+        aposiciones.add(coordenada);
+        for (int i =0; i<8 ;i++){
+            
+            aposiciones.add(new Posicion(i,y));
+            
+        }
+        for (int j =0; j<'a' ; j++){
+            
+            
+            int jn= Character.getNumericValue(j);
+            jn = jn + 1;
+            char jc = (char)(jn);
+            
+            
+            aposiciones.add(new Posicion(x,jc));
+        }
+        
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

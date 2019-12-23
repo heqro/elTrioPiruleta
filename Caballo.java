401936495a3f0897c82f6888a1eb2445c71dd1bf
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-23 18:29:36 +0100

    Update Caballo.java

--- Caballo.java
+++ Caballo.java
@@ -13,7 +13,96 @@ public class Caballo extends Pieza{
     }
 
     @Override
-    public ArrayList<Posicion> calcularMovimientos() {
+    public ArrayList<Posicion> calcularMovimientos(Posicion coordenada) {
+        ArrayList<Posicion> aposiciones = new ArrayList();
+        int x  = coordenada.getCoordenadax();
+        char y = coordenada.getCoordenaday();
+        aposiciones.add(coordenada);
+        
+        int xaux= x;
+        char yaux = y;
+        int yn = Character.getNumericValue(yaux);
+        xaux=xaux+3;
+        yn=yn+1;
+        char yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux+3;
+        yn=yn-1;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux-3;
+        yn=yn+1;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux-3;
+        yn=yn-1;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux+1;
+        yn=yn+3;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux+1;
+        yn=yn-3;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux+1;
+        yn=yn-3;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
+        
+        
+        xaux= x;
+        yaux = y;
+        yn = Character.getNumericValue(yaux);
+        xaux=xaux-1;
+        yn=yn-3;
+        yc = (char) yn;
+        if (xaux<9 && xaux>0 && yc<='h' && yc >='a'){
+            aposiciones.add(new Posicion(xaux,yc));
+        }
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

b7e02c7025927cad9819d8f1ae8828ce18bc7e6b
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-23 13:03:49 +0100

    Update Dama.java

--- Dama.java
+++ Dama.java
@@ -13,7 +13,69 @@ public class Dama extends Pieza{
     }
 
     @Override
-    public ArrayList<Posicion> calcularMovimientos() {
+    public ArrayList<Posicion> calcularMovimientos(Posicion coordenada) {
+        ArrayList<Posicion> aposiciones = new ArrayList();
+        int x  = coordenada.getCoordenadax();
+        char y = coordenada.getCoordenaday();
+        int xaux=x;
+        char yaux=y;
+        aposiciones.add(coordenada);
+        
+        while( x < 8 ||  y< 'h'){ //diagonal sup derecha
+            x= x+1;
+            int yn= Character.getNumericValue(y);
+            yn=yn + 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        x=xaux;
+        y=yaux;
+        while( x < 8 ||  y> 'a'){ //diagonal sup izda
+            x= x+1;
+            int yn= Character.getNumericValue(y);
+            yn=yn - 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        x=xaux;
+        y=yaux;
+        while( x > 1 ||  y< 'h'){ //diagonal inf derecha
+            x= x-1;
+            int yn= Character.getNumericValue(y);
+            yn=yn + 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        x=xaux;
+        y=yaux;
+        while( x > 1 ||  y> 'a'){ //diagonal inf izquierda
+            x= x-1;
+            int yn= Character.getNumericValue(y);
+            yn=yn - 1;
+            y = (char)yn;
+            aposiciones.add(new Posicion(x,y));
+            
+        }
+        x=xaux;
+        y=yaux;
+         for (int i =0; i<8 ;i++){
+            
+            aposiciones.add(new Posicion(i,y));
+            
+        }
+        for (int j =0; j<'a' ; j++){
+            
+            
+            int jn= Character.getNumericValue(j);
+            jn = jn + 1;
+            char jc = (char)(jn);
+            
+            
+            aposiciones.add(new Posicion(x,jc));
+        }
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

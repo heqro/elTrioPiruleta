0560af464160913f2b3d5df8d5f033233499c331
SlavaStalin <58432330+SlavaStalin@users.noreply.github.com>
2019-12-27 10:08:18 +0100

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -5,6 +5,11 @@
  */
 package p_final;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author juans
@@ -18,6 +23,60 @@ public class Usuario {
         nombre=n;
         password=p;
         ModelosUsuario=m;
+    }
+    public void leerEjemplo(String archivo)throws FileNotFoundException, IOException{ //esta cadena es la ruta donde esta el txt
+      String cadena;
+      int cfila=7; //contador fila
+      char c;
+      int ccolumna=0;// contador columna
+      
+      
+      Pieza Marcador[][] = new Pieza[8][8]; 
+      Tablero tablero = new Tablero(Marcador);// marcador sera nuestro tablero
+      FileReader f = new FileReader(archivo);
+      BufferedReader b = new BufferedReader(f);
+      while((cadena = b.readLine())!=null) {
+          
+          // b es la cadena que tiene la primera linea del tablero empezando por arriba
+          for(int i=0; i<cadena.length();i++){
+              if(cadena.charAt(i)!=','){
+                  switch(cadena.charAt(i)){
+                      case 'V': Marcador[cfila][ccolumna]=null;
+                                break;
+                      case 'R': Marcador[cfila][ccolumna]=new Rey(cadena.charAt(i+1),tablero);
+                                i=i+1; //sabemos que el siguiente caracter a leer es el tipo de pieza asi que lo adelantamos intencionadamente para tener un buen control del contador de columnas
+                                break;
+                      case 'P': Marcador[cfila][ccolumna]=new Rey(cadena.charAt(i+1),tablero);
+                                i=i+1;
+                                break;
+                      case 'C': Marcador[cfila][ccolumna]=new Rey(cadena.charAt(i+1),tablero);
+                                i=i+1;
+                                break;
+                      case 'T': Marcador[cfila][ccolumna]=new Rey(cadena.charAt(i+1),tablero);
+                                i=i+1;
+                                break;    
+                      case 'A': Marcador[cfila][ccolumna]=new Rey(cadena.charAt(i+1),tablero);
+                                i=i+1;
+                                break;
+                      case 'D': Marcador[cfila][ccolumna]=new Rey(cadena.charAt(i+1),tablero);
+                                i=i+1;
+                                break;
+                                
+                  } 
+                  ccolumna=ccolumna+1;
+              }
+          
+          }
+          cfila=cfila-1;
+          ccolumna=0;
+      }
+      b.close();
+      //Ahora crearemos el modelo correspondiente teniendo ya el tablero
+      //La jugada ganadora sera siempre el ultimo string que sacamos de b
+      Modelo modelo = new Modelo(tablero,cadena); //nuestro modelo
+      this.ModelosUsuario.add(new ModeloUsuario(modelo,false,0,0)); //ya lo hemos añadido al almacen incializado
+      
+      
     }
     public String getNombre(){
         return nombre;

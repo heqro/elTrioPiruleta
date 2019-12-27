339b65d4e0eb13931f745e43ee9e1d7b63b3c885
heqro <58432067+heqro@users.noreply.github.com>
2019-12-27 19:09:13 +0100

    Update Alfil.java

--- Alfil.java
+++ Alfil.java
@@ -13,96 +13,99 @@ public class Alfil extends Pieza{
         super(new Color(color), 'A', t, pos);
      }
 
-    public ArrayList<Posicion> calcularMovimientos(Tablero t) {
-        
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     @Override
-    public void calcularMovimientos() {
-        Posicion coordenada = this.getPosicion();
+    public void calcularMovimientos(){
         ArrayList<Posicion> aposiciones = new ArrayList();
-        int x  = coordenada.getCoordenadax();
-        char y = coordenada.getCoordenaday();
-        int xaux=x;
-        char yaux=y;
         Tablero t = this.getTablero();
-        boolean posNoOcupada;
-        aposiciones.add(coordenada);
-        
-        int yn;
-        
-        posNoOcupada = true;
-        while( (x < 8 ||  y< 'h')&& posNoOcupada){ //diagonal sup derecha
-            x= x+1;
-            yn= Character.getNumericValue(y);
-            yn=yn + 1;
-            y = (char)yn;
-            if (!(t.PosicionOcupada(x,y))){
-                aposiciones.add(new Posicion(x,y));}
-            else if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
-                aposiciones.add(new Posicion(x,y));
-                posNoOcupada = false;
-            }
-            else {posNoOcupada = false;}
-            
-        }
-        x=xaux;
-        y=yaux;
-        posNoOcupada = true;
-        while( x < 8 ||  y> 'a'){ //diagonal sup izda
-            x= x+1;
-            yn= Character.getNumericValue(y);
-            yn=yn - 1;
-            
-            
-            y = (char)yn;
-            if (!(t.PosicionOcupada(x,y))){
-                aposiciones.add(new Posicion(x,y));}
-            else{
-                if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
-                aposiciones.add(new Posicion(x,y));
-                posNoOcupada = false;
-                }
-                else {posNoOcupada = false;}
-            
-            
-        }
-        x=xaux;
-        y=yaux;
-        posNoOcupada = true;
-        while( x > 1 ||  y< 'h'){ //diagonal inf derecha
-            x= x-1;
-            yn= Character.getNumericValue(y);
-            yn=yn + 1;
-            y = (char)yn;
-            if (!(t.PosicionOcupada(x,y))){
-                aposiciones.add(new Posicion(x,y));}
-            else if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
-                aposiciones.add(new Posicion(x,y));
-                posNoOcupada = false;
-            }
-            else {posNoOcupada = false;}
-            
-        }
-        posNoOcupada = true;
-        x=xaux;
-        y=yaux;
-        while( x > 1 ||  y> 'a'){ //diagonal inf izquierda
-            x= x-1;
-            yn= Character.getNumericValue(y);
-            yn=yn - 1;
-            y = (char)yn;
-           if (!(t.PosicionOcupada(x,y))){
-                aposiciones.add(new Posicion(x,y));}
-            else if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
-                aposiciones.add(new Posicion(x,y));
-                posNoOcupada = false;
-            }
-            else {posNoOcupada = false;}
-            
-        }
+        aposiciones = t.lecturaAlfil(this.getColor(), this.getPosicion());
+        this.setPosiblesMovimientos(aposiciones);
     }
+    
+//    @Override
+//    public void calcularMovimientos() {
+//        Posicion coordenada = this.getPosicion();
+//        ArrayList<Posicion> aposiciones = new ArrayList();
+//        int x  = coordenada.getCoordenadax();
+//        char y = coordenada.getCoordenaday();
+//        int xaux=x;
+//        char yaux=y;
+//        Tablero t = this.getTablero();
+//        boolean posNoOcupada;
+//        aposiciones.add(coordenada);
+//        
+//        int yn;
+//        
+//        posNoOcupada = true;
+//        while( (x < 8 ||  y< 'h')&& posNoOcupada){ //diagonal sup derecha
+//            x= x+1;
+//            yn= Character.getNumericValue(y);
+//            yn=yn + 1;
+//            y = (char)yn;
+//            if (!(t.PosicionOcupada(x,y))){
+//                aposiciones.add(new Posicion(x,y));}
+//            else if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
+//                aposiciones.add(new Posicion(x,y));
+//                posNoOcupada = false;
+//            }
+//            else {posNoOcupada = false;}
+//            
+//        }
+//        x=xaux;
+//        y=yaux;
+//        posNoOcupada = true;
+//        while( x < 8 ||  y> 'a'){ //diagonal sup izda
+//            x= x+1;
+//            yn= Character.getNumericValue(y);
+//            yn=yn - 1;
+//            
+//            
+//            y = (char)yn;
+//            if (!(t.PosicionOcupada(x,y))){
+//                aposiciones.add(new Posicion(x,y));}
+//            else{
+//                if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
+//                aposiciones.add(new Posicion(x,y));
+//                posNoOcupada = false;
+//                }
+//                else {posNoOcupada = false;}
+//            
+//            
+//        }
+//        x=xaux;
+//        y=yaux;
+//        posNoOcupada = true;
+//        while( x > 1 ||  y< 'h'){ //diagonal inf derecha
+//            x= x-1;
+//            yn= Character.getNumericValue(y);
+//            yn=yn + 1;
+//            y = (char)yn;
+//            if (!(t.PosicionOcupada(x,y))){
+//                aposiciones.add(new Posicion(x,y));}
+//            else if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
+//                aposiciones.add(new Posicion(x,y));
+//                posNoOcupada = false;
+//            }
+//            else {posNoOcupada = false;}
+//            
+//        }
+//        posNoOcupada = true;
+//        x=xaux;
+//        y=yaux;
+//        while( x > 1 ||  y> 'a'){ //diagonal inf izquierda
+//            x= x-1;
+//            yn= Character.getNumericValue(y);
+//            yn=yn - 1;
+//            y = (char)yn;
+//           if (!(t.PosicionOcupada(x,y))){
+//                aposiciones.add(new Posicion(x,y));}
+//            else if (!this.getColor().equals(t.GetPiezaPos(x, y).getColor())){
+//                aposiciones.add(new Posicion(x,y));
+//                posNoOcupada = false;
+//            }
+//            else {posNoOcupada = false;}
+//            
+//        }
+//    }
     
     
 //    @Override

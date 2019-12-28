6644c51ddc9aef3b7c259bf8f4fb0acd18bdabf7
heqro <58432067+heqro@users.noreply.github.com>
2019-12-28 22:29:23 +0100

    Update Caballo.java

--- Caballo.java
+++ Caballo.java
@@ -21,7 +21,8 @@ public class Caballo extends Pieza{
             5. Si el color de la pieza es el mismo que el de la pieza, excluiremos
         el movimiento.*/ 
         
-        ArrayList<Posicion> aposiciones = new ArrayList();
+        ArrayList<Posicion> aposiciones = new ArrayList<>();
+        ArrayList<Posicion> posiblesMovimientos = new ArrayList<>();
         Posicion coordenada = this.getPosicion();
         int x = coordenada.getCoordenadax();
         char y = coordenada.getCoordenaday();
@@ -41,238 +42,24 @@ public class Caballo extends Pieza{
         aposiciones.add(new Posicion((x+1), (char)(y-2)));
         
         for(Posicion pos: aposiciones){
-            if(!pos.posicionLegal()){
-                aposiciones.remove(pos);
-            }else{
+            if(pos.posicionLegal()){
+                /*Solo vamos a introducir aquellas posiciones que sean legales*/
                 if(t.PosicionOcupada(pos)){
+                    /*Si la posición está ocupada, queremos saber si es
+                    por una pieza del contrario o por una nuestra*/
                     Color c = t.GetPiezaPos(pos).getColor();
-                    if(c.equals(this.getColor())){
-                        aposiciones.remove(pos);
+                    if(!c.equals(this.getColor())){
+                        /*Si los colores son distintos, introducimos la posición
+                        en el array de posibles movimientos*/
+                        posiblesMovimientos.add(pos);
                     }
+                }else{
+                    /*Si la posición no está ocupada, entonces la añadimos
+                    en el array de posibles movimientos automáticamente*/
+                    posiblesMovimientos.add(pos);
                 }
             }
         }
+        this.setPosiblesMovimientos(posiblesMovimientos);
     }
-//    @Override
-//    public void calcularMovimientos() {
-//        ArrayList<Posicion> aposiciones = new ArrayList();
-//        Posicion coordenada = this.getPosicion();
-//        int x  = coordenada.getCoordenadax();
-//        char y = coordenada.getCoordenaday();
-//        Color c;
-//        aposiciones.add(coordenada);
-//        
-//        int xaux= x;
-//        char yaux = y;
-//        int yn = Character.getNumericValue(yaux);
-//        xaux=xaux+3;
-//        yn=yn+1;       
-//        char yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+3;
-//        yn=yn-1;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux-3;
-//        yn=yn+1;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& !(this.getColor.equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux-3;
-//        yn=yn-1;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+1;
-//        yn=yn+3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a') && !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+1;
-//        yn=yn-3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+1;
-//        yn=yn-3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux-1;
-//        yn=yn-3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a') && !(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-
-//    @Override
-//    public ArrayList<Posicion> calcularMovimientos(Posicion coordenada) {
-//        ArrayList<Posicion> aposiciones = new ArrayList();
-//        int x  = coordenada.getCoordenadax();
-//        char y = coordenada.getCoordenaday();
-//        Color c;
-//        aposiciones.add(coordenada);
-//        
-//        int xaux= x;
-//        char yaux = y;
-//        int yn = Character.getNumericValue(yaux);
-//        xaux=xaux+3;
-//        yn=yn+1;       
-//        char yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& not(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+3;
-//        yn=yn-1;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& not(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux-3;
-//        yn=yn+1;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& not(this.getColor.equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux-3;
-//        yn=yn-1;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& not (this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+1;
-//        yn=yn+3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a') && not (this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+1;
-//        yn=yn-3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& not(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux+1;
-//        yn=yn-3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a')&& not(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        
-//        
-//        xaux= x;
-//        yaux = y;
-//        yn = Character.getNumericValue(yaux);
-//        xaux=xaux-1;
-//        yn=yn-3;
-//        yc = (char) yn;
-//        if (t.PosicionOcupada(xaux,yn)){c=t.GetPiezaPos(xaux,yaux).getColor()}
-//        else{c = null};
-//        if ((xaux<9 && xaux>0 && yc<='h' && yc >='a') && not(this.getColor().equals(c))){
-//            aposiciones.add(new Posicion(xaux,yc));
-//        }
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
 }

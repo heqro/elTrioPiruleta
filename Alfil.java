52fcb83a350e7387ae500dfca49a1bb2f78154b1
heqro <58432067+heqro@users.noreply.github.com>
2019-12-31 11:17:07 +0100

    Update Alfil.java

--- Alfil.java
+++ Alfil.java
@@ -13,10 +13,11 @@ public class Alfil extends Pieza{
             Etiquetas:
             (1) Recogemos el mensaje de tipo Tablero asociado al objeto de tipo Alfil
             (2) Utilizaremos un array auxiliar sobre el que guardaremos el resultado de un
-            método 
+            método para leer en diagonal del propio tablero
+            (3) Asignaremos al mensaje de tipo Tablero el nuevo array de posibles posiciones
         */
-        Tablero t = this.getTablero();/*Recogemos el tablero asociado a la pieza*/
-        ArrayList<Posicion> aposiciones = t.lecturaAlfil(this.getColor(), this.getPosicion());/**/
-        this.setPosiblesMovimientos(aposiciones);
+        Tablero t = this.getTablero();//(1)
+        ArrayList<Posicion> aposiciones = t.lecturaAlfil(this.getColor(), this.getPosicion());//(2)
+        this.setPosiblesMovimientos(aposiciones);//
     }
 }
